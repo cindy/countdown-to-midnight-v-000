fabd12a5c8d4539dfa6cca89bdc8f63340256a93
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-16 00:45:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,10 @@ def countdown(num)
 end
 
 def countdown_with_sleep(num)
-  countdown(num)
-  sleep(num)
+  until num == 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
